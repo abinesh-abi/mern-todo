f6afa1b1c24275789b8e5e5cadd88a3331025cfa
abi <introvertedabi@gmail.com>
2022-02-15 21:17:49 +0530

    add item added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import React,{useState,useEffect} from 'react'
+import Addtask from './Components/Addtask'
+import axios from 'axios'
+import './App.css'
+
 function App() {
+  const [todolist,settodolist]=useState([])
+  
+  useEffect(()=>{
+    axios.get('http://localhost:3333')
+    .then(res=>{
+      settodolist(res.data)
+    }).catch(err=>console.log(err))
+  },[])
+  const addTask = newTask =>{
+    settodolist([...todolist,newTask])
+  }
   return (
     <div className="App">
-    <h1>test</h1>
+    <Addtask addTask= {addTask} />
     </div>
   );
 }
