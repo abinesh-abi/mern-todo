1f12edb8a100ce84dc4f18fecb3118a1a0ec8b66
abi <introvertedabi@gmail.com>
2022-02-15 16:23:13 +0530

    serverr finished

--- server/routes/routers.js
+++ server/routes/routers.js
@@ -15,5 +15,20 @@ router.post('/',(req,res)=>{
   .catch(err=>console.log(err))
 })
 
+router.put('/:id',(req,res)=>{
+  Task.findOneAndUpdate({
+    _id:req.params.id
+  },req.body,{new:true},(err,doc)=>{
+    if(err)console.log(err);
+    res.json(doc)
+  })
+})
+router.delete('/:id',(req,res)=>{
+  Task.findByIdAndDelete(req.params.id,(err,doc)=>{
+    if(err)console.log(err);
+    res.json(doc)
+  })
+})
+
 
 module.exports = router;
